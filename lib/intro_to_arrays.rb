2768bf4f034779a260072c7a4c5929e79ae381f1
Dennis <dennis.basyrov@gmail.com>
2019-05-12 03:06:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,7 +6,12 @@ def array_with_two_elements()
   @my_two_array = [1, 2]
 end  
 
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
 def first_element(i)
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   return i[0]
+end  
+
+def first_element(i)
+  return i[2]
 end  
