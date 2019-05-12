8d5df224eabad097028f63eb6955ed087613e202
Dennis <dennis.basyrov@gmail.com>
2019-05-12 03:17:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -34,5 +34,6 @@ def last_element_with_array_methods(p)
 end 
 
 def length_of_array()
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
   
 end  
