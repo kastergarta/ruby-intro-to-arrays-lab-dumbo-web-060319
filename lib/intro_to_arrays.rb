e3eefbe5e8a1bc1744e39a90e8b345c5f61cd17a
Dennis <dennis.basyrov@gmail.com>
2019-05-12 03:10:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,8 +9,8 @@ end
 
 
 def first_element(i)
-  # # @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  # return i[0]
+  # @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  return i[0]
 end  
 
 def first_element(m)
